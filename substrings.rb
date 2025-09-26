272abd091cc8260e7a9ed4db18898c2959876a76
johnpaulcurtin <juanlove@gmail.com>
2025-09-26 15:45:22 -0700

    create beginnings of method

---
+++ substrings.rb
@@ -0,0 +1,15 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def sub_strings (sentence, dictionary)
+    sentence_split = sentence.gsub(/[^a-zA-Z0-9\s]/, "").split
+    
+
+
+
+
+
+end
+
+sub_strings("below the horn!", dictionary)
+
+# create method to  return a hash that matches and counts each instance of a given word
