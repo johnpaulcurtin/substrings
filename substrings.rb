8da43c3bc8b26ff6b9bec2f9236abc6532b92b0a
johnpaulcurtin <juanlove@gmail.com>
2025-10-04 15:39:45 -0700

    create method for substring comparison vs dictonary allow for user input

--- substrings.rb
+++ substrings.rb
@@ -1,15 +1,22 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def sub_strings (sentence, dictionary)
-    sentence_split = sentence.gsub(/[^a-zA-Z0-9\s]/, "").split
-    
+puts 'Please enter text:'
+sentence = gets.chomp.to_s
 
+
+def sub_strings (sentence, dictionary)
+    result = Hash.new(0)
+    sentence_split = sentence.gsub(/[^a-zA-Z0-9\s]/, "").downcase.split
+    sentence_split.each do |str|
+      result[str] += 1 if dictionary.include?(str)
+    end
+    p result
 
 
 
 
 end
 
-sub_strings("below the horn!", dictionary)
+sub_strings(sentence, dictionary)
 
 # create method to  return a hash that matches and counts each instance of a given word
